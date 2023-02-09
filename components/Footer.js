ad554de13288f220a7b8b5a61cb3814bee0698a3
0xSunil <sunday7637@gmail.com>
2023-02-09 17:51:42 +0530

    added contact section

--- components/Footer.js
+++ components/Footer.js
@@ -3,28 +3,43 @@ import Link from "next/link";
 import { BsInstagram } from "react-icons/bs";
 import { BsTwitter } from "react-icons/bs";
 import { AiTwotoneMail } from "react-icons/ai";
-const Footer = (props) => {
+const Footer = ({ insta, twitter, mail }) => {
   return (
     <section id="contect" className="mt-auto ">
-      <div className="flex flex-row justify-around border-t-2 border-yellow-300 sm:px-32 px-6 py-6 sm:py-10 sm:mx-10 mx-5 bottom-0 right-0 left-0 align-bottom">
-        <Link
-          href="https://www.instagram.com/sunil.eth/"
-          className="text-3xl text-yellow-300 hover:text-yellow-700"
-        >
-          <BsInstagram />
-        </Link>
-        <Link
-          href="https://twitter.com/0xSunilReddy"
-          className="text-3xl  text-yellow-300 hover:text-yellow-700"
-        >
-          <BsTwitter />
-        </Link>
-        <Link
-          href="mailto:sunday7637@gmail.com"
-          className="text-3xl   text-yellow-300 hover:text-yellow-700"
-        >
-          <AiTwotoneMail />
-        </Link>
+      <div className="flex flex-row justify-around border-yellow-300 sm:px-32 px-6 py-6 sm:py-10 sm:mx-10 mx-5 bottom-0 right-0 left-0 align-bottom">
+        {insta && (
+          <Link
+            href={insta}
+            className="text-3xl text-yellow-300 hover:text-yellow-700"
+          >
+            <BsInstagram />
+          </Link>
+        )}
+        {twitter && (
+          <Link
+            href={twitter}
+            className="text-3xl  text-yellow-300 hover:text-yellow-700"
+          >
+            <BsTwitter />
+          </Link>
+        )}
+
+        {mail && (
+          <Link
+            href={`mailto:${mail}`}
+            className="text-3xl   text-yellow-300 hover:text-yellow-700"
+          >
+            <AiTwotoneMail />
+          </Link>
+        )}
+        {mail && (
+          <Link
+            href={`mailto:${mail}`}
+            className="text-3xl   text-yellow-300 hover:text-yellow-700"
+          >
+            <AiTwotoneMail />
+          </Link>
+        )}
       </div>
     </section>
   );
--- pages/contect.js
+++ pages/contect.js
@@ -1,15 +1,31 @@
-import ContentForm from "@/components/contect/ContentForm";
-// import Footer from "@/components/footer";
+import Footer from "@/components/footer";
 import NavBar from "@/components/NavBar";
-import React from "react";
 
 const contect = () => {
   return (
     <section className="bg-black xl:px-0 sm:px-16 px-6  ">
       <NavBar con={false} cont={false} allp={true} />
-      <ContentForm />
-
-      {/* <Footer /> */}
+      {/* <ContentForm /> */}
+      <div className="mb-28">
+        <h1 className="font-1 font-poppins font-semibold sm:text-[52px] text-[25px] text-white  text-center">
+          Contact me
+        </h1>
+        <Footer
+          insta="https://www.instagram.com/sunil.eth/"
+          twitter="https://twitter.com/0xSunilReddy"
+          mail="sunday7637@gmail.com"
+        />
+      </div>
+      <div>
+        <h1 className="font-1 font-poppins font-semibold sm:text-[52px] text-[25px] text-white  text-center">
+          Contact Sol Casino
+        </h1>
+        <Footer
+        
+          twitter="https://twitter.com/Solcasinoio"
+          mail="support@solcasino.io"
+        />
+      </div>
     </section>
   );
 };
